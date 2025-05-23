4ab6d379c339699f806bc928bd2c1a431ed67d9d
Harsh <harsh.devavanshi@autozone.com>
2025-05-23 12:00:55 +0530

    Configure for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? '' : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
@@ -7,6 +10,8 @@ const nextConfig: NextConfig = {
   },
   trailingSlash: true,
   distDir: 'out',
+  basePath,
+  assetPrefix: isProd ? '/' : '',
 };
 
 export default nextConfig;
--- src/app/util/helper.ts
+++ src/app/util/helper.ts
@@ -1,1 +1,4 @@
-export const basePath = process.env.NODE_ENV === 'production' ? '/erazone' : '';
+// For local development, GitHub Pages, and custom domain
+export const basePath = process.env.NODE_ENV === 'production' 
+  ? (typeof window !== 'undefined' && window.location.hostname === 'developerharsh.com' ? '' : '/erazone')
+  : '';
